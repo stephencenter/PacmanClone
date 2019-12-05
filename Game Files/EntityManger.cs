2721dc2e65234225139efa2838218887b7cd3835
stephencenter <stephenbcenter@gmail.com>
2019-12-04 23:12:18 -0500

    Fully programmed Blinky AI, reworked controls to be more similar to normal Pacman

--- Game Files/EntityManger.cs
+++ Game Files/EntityManger.cs
@@ -30,23 +30,26 @@ namespace Pacman
             inky.Sprite = content.Load<Texture2D>("inky");
             clyde.Sprite = content.Load<Texture2D>("clyde");
 
-            player.PosX = 218;
-            player.PosY = 418;
+            player.PosX = (int)(13.3*Pacman.tile_size);
+            player.PosY = 26*Pacman.tile_size;
+
+            blinky.PosX = 26*Pacman.tile_size;
+            blinky.PosY = 4*Pacman.tile_size;
+
+            pinky.PosX = 1*Pacman.tile_size;
+            pinky.PosY = 4*Pacman.tile_size;
+
+            inky.PosX = 26*Pacman.tile_size;
+            inky.PosY = 32*Pacman.tile_size;
+
+            clyde.PosX = 1*Pacman.tile_size;
+            clyde.PosY = 32*Pacman.tile_size;
         }
-    }
 
-    public abstract class Entity
-    {
-        public int PosX { get; set; }
-        public int PosY { get; set; }
-        public Texture2D Sprite { get; set; }
-
-        public abstract void Move();
-
-        public List<Tile> GetCurrentTiles()
+        public static List<Tile> GetCurrentTiles(int pos_x, int pos_y)
         {
-            Vector2 e_top_left = new Vector2(PosX, PosY);
-            Vector2 e_bot_right = new Vector2(PosX + Pacman.player_size, PosY + Pacman.player_size);
+            Vector2 e_top_left = new Vector2(pos_x, pos_y);
+            Vector2 e_bot_right = new Vector2(pos_x + Pacman.entity_size, pos_y + Pacman.entity_size);
 
             List<Tile> current_tiles = new List<Tile>();
             foreach (Tile tile in TileManager.GetTileList())
@@ -67,19 +70,59 @@ namespace Pacman
             return current_tiles;
         }
 
-        public bool DoObjectsOverlap(Vector2 tl1, Vector2 tl2, Vector2 br1, Vector2 br2)
+        public static bool DoObjectsOverlap(Vector2 tl1, Vector2 tl2, Vector2 br1, Vector2 br2)
         {
             return tl1.X < br2.X && tl2.X < br1.X && tl1.Y < br2.Y && tl2.Y < br1.Y;
         }
+    }
 
-        public bool IsWithinNonTraversable()
+    public abstract class Entity
+    {
+        public int PosX { get; set; }
+        public int PosY { get; set; }
+        public Texture2D Sprite { get; set; }
+        public Pacman.Button? CurrentAction { get; set; }
+        public Pacman.Button? QueuedAction { get; set; }
+
+        public abstract void Move();
+
+        public bool PredictCollision(int x_delta, int y_delta)
         {
-            return GetCurrentTiles().Any(x => !x.Traversable);
+            return EntityManager.GetCurrentTiles(PosX + x_delta, PosY + y_delta).Any(x => !x.Traversable);
         }
 
         public bool IsWithinWarp()
         {
-            return GetCurrentTiles().Any(x => x is Warp);
+            return EntityManager.GetCurrentTiles(PosX, PosY).Any(x => x is Warp);
+        }
+
+        public void WarpEntity()
+        {
+            Warp the_warp = EntityManager.GetCurrentTiles(PosX, PosY).First(x => x is Warp) as Warp;
+            Warp matching_warp = TileManager.FindMatchingWarp(the_warp);
+            if (CurrentAction == Pacman.Button.move_up)
+            {
+                PosX = matching_warp.PosX;
+                PosY = matching_warp.PosY - Pacman.tile_size;
+            }
+
+            if (CurrentAction == Pacman.Button.move_down)
+            {
+                PosX = matching_warp.PosX;
+                PosY = matching_warp.PosY + Pacman.tile_size;
+            }
+
+            if (CurrentAction == Pacman.Button.move_left)
+            {
+                PosX = matching_warp.PosX - Pacman.tile_size;
+                PosY = matching_warp.PosY;
+            }
+
+            if (CurrentAction == Pacman.Button.move_right)
+            {
+                PosX = matching_warp.PosX + Pacman.tile_size;
+                PosY = matching_warp.PosY;
+            }
         }
     }
 
@@ -87,107 +130,272 @@ namespace Pacman
     {
         public override void Move()
         {
-            const int delta_move = 2;
+            const int delta_move = 3;
 
-            if (Logic.IsOnlyOneDirectionPressed())
+            // Get keyboard input to determine current and queued actions
+            if (Logic.IsButtonPressed(Pacman.Button.move_up))
             {
-                if (Logic.IsButtonPressed(Pacman.Button.move_up))
+                if ((CurrentAction == Pacman.Button.move_left || CurrentAction == Pacman.Button.move_right) && PredictCollision(0, -delta_move))
+                {
+                    QueuedAction = Pacman.Button.move_up;
+                }
+
+                else
+                {
+                    CurrentAction = Pacman.Button.move_up;
+                }
+            }
+
+            else if (Logic.IsButtonPressed(Pacman.Button.move_down))
+            {
+                if ((CurrentAction == Pacman.Button.move_left || CurrentAction == Pacman.Button.move_right) && PredictCollision(0, delta_move))
+                {
+                    QueuedAction = Pacman.Button.move_down;
+                }
+
+                else
+                {
+                    CurrentAction = Pacman.Button.move_down;
+                }
+            }
+
+            else if (Logic.IsButtonPressed(Pacman.Button.move_left))
+            {
+                if ((CurrentAction == Pacman.Button.move_up || CurrentAction == Pacman.Button.move_down) && PredictCollision(-delta_move, 0))
+                {
+                    QueuedAction = Pacman.Button.move_left;
+                }
+
+                else
+                {
+                    CurrentAction = Pacman.Button.move_left;
+                }
+            }
+
+            else if (Logic.IsButtonPressed(Pacman.Button.move_right))
+            {
+                if ((CurrentAction == Pacman.Button.move_up || CurrentAction == Pacman.Button.move_down) && PredictCollision(delta_move, 0))
+                {
+                    QueuedAction = Pacman.Button.move_right;
+                }
+
+                else
+                {
+                    CurrentAction = Pacman.Button.move_right;
+                }
+            }
+
+            // Execute the current and queued actions
+            if (CurrentAction == Pacman.Button.move_up || (QueuedAction == Pacman.Button.move_up && !PredictCollision(0, -delta_move)))
+            {
+                if (!PredictCollision(0, -delta_move))
                 {
                     PosY -= delta_move;
-                    if (IsWithinNonTraversable())
-                    {
-                        PosY += delta_move;
-                    }
-                }
 
-                else if (Logic.IsButtonPressed(Pacman.Button.move_down))
-                {
-                    PosY += delta_move;
-                    if (IsWithinNonTraversable())
+                    if (QueuedAction == Pacman.Button.move_up)
                     {
-                        PosY -= delta_move;
-                    }
-                }
-
-                else if (Logic.IsButtonPressed(Pacman.Button.move_left))
-                {
-                   PosX -= delta_move;
-                    if (IsWithinNonTraversable())
-                    {
-                        PosX += delta_move;
-                    }
-                }
-
-                else if (Logic.IsButtonPressed(Pacman.Button.move_right))
-                {
-                    PosX += delta_move;
-                    if (IsWithinNonTraversable())
-                    {
-                        PosX -= delta_move;
+                        CurrentAction = Pacman.Button.move_up;
+                        QueuedAction = null;
                     }
                 }
 
                 else
                 {
-                    return;
+                    CurrentAction = null;
                 }
+            }
 
-                if (IsWithinWarp())
+            else if (CurrentAction == Pacman.Button.move_down || (QueuedAction == Pacman.Button.move_down && !PredictCollision(0, delta_move)))
+            {
+                if (!PredictCollision(0, delta_move))
                 {
-                    Warp the_warp = GetCurrentTiles().First(x => x is Warp) as Warp;
-                    Warp matching_warp = TileManager.FindMatchingWarp(the_warp);
-                    if (Logic.IsButtonPressed(Pacman.Button.move_up))
-                    {
-                        PosX = matching_warp.PosX;
-                        PosY = matching_warp.PosY - Pacman.tile_size;
-                    }
+                    PosY += delta_move;
 
-                    if (Logic.IsButtonPressed(Pacman.Button.move_down))
+                    if (QueuedAction == Pacman.Button.move_down)
                     {
-                        PosX = matching_warp.PosX;
-                        PosY = matching_warp.PosY + Pacman.tile_size;
-                    }
-                    if (Logic.IsButtonPressed(Pacman.Button.move_left))
-                    {
-                        PosX = matching_warp.PosX - Pacman.tile_size;
-                        PosY = matching_warp.PosY;
-                    }
-
-                    if (Logic.IsButtonPressed(Pacman.Button.move_right))
-                    {
-                        PosX = matching_warp.PosX + Pacman.tile_size;
-                        PosY = matching_warp.PosY;
+                        CurrentAction = Pacman.Button.move_down;
+                        QueuedAction = null;
                     }
                 }
+
+                else
+                {
+                    CurrentAction = null;
+                }
+            }
+
+            else if (CurrentAction == Pacman.Button.move_left || (QueuedAction == Pacman.Button.move_left && !PredictCollision(-delta_move, 0)))
+            {
+                if (!PredictCollision(-delta_move, 0))
+                {
+                    PosX -= delta_move;
+
+                    if (QueuedAction == Pacman.Button.move_left)
+                    {
+                        CurrentAction = Pacman.Button.move_left;
+                        QueuedAction = null;
+                    }
+                }
+
+                else
+                {
+                    CurrentAction = null;
+                }
+            }
+
+            else if (CurrentAction == Pacman.Button.move_right || (QueuedAction == Pacman.Button.move_right && !PredictCollision(delta_move, 0)))
+            {
+                if (!PredictCollision(delta_move, 0))
+                {
+                    PosX += delta_move;
+
+                    if (QueuedAction == Pacman.Button.move_right)
+                    {
+                        CurrentAction = Pacman.Button.move_right;
+                        QueuedAction = null;
+                    }
+
+                }
+
+                else
+                {
+                    CurrentAction = null;
+                }
+            }
+
+            if (IsWithinWarp())
+            {
+                WarpEntity();
             }
         }
     }
 
     public abstract class Ghost : Entity
     {
+        Pacman.Direction FacingDirection = Pacman.Direction.down;
+
         public override void Move()
         {
+            //if (!(this is Blinky))
+            //{
+            //    return;
+            //}
 
+            Vector2 target = GetTarget();
+
+            const int delta_move = 2;
+
+            float dist_up = (float)Math.Sqrt(Math.Pow(PosX - target.X, 2) + Math.Pow(PosY - delta_move - target.Y, 2));
+            float dist_down = (float)Math.Sqrt(Math.Pow(PosX - target.X, 2) + Math.Pow(PosY + delta_move - target.Y, 2));
+            float dist_left = (float)Math.Sqrt(Math.Pow(PosX - delta_move - target.X, 2) + Math.Pow(PosY - target.Y, 2));
+            float dist_right = (float)Math.Sqrt(Math.Pow(PosX + delta_move - target.X, 2) + Math.Pow(PosY - target.Y, 2));
+
+            Dictionary<Pacman.Direction, float> distances = new Dictionary<Pacman.Direction, float>() 
+            {
+                { Pacman.Direction.up, dist_up },
+                { Pacman.Direction.down, dist_down },
+                { Pacman.Direction.left, dist_left },
+                { Pacman.Direction.right, dist_right }
+            };
+
+            distances.Remove(Pacman.OppositeDir[FacingDirection]);
+            while (true)
+            {
+                if (distances.Values.Min() == dist_up && distances.Keys.Contains(Pacman.Direction.up))
+                {
+                    if (PredictCollision(0, -delta_move))
+                    {
+                        distances.Remove(Pacman.Direction.up);
+                        continue;
+                    }
+
+                    PosY -= delta_move;
+                    FacingDirection = Pacman.Direction.up;
+                    CurrentAction = Pacman.Button.move_up;
+                    break;
+                }
+
+                if (distances.Values.Min() == dist_down && distances.Keys.Contains(Pacman.Direction.down))
+                {
+                    if (PredictCollision(0, delta_move))
+                    {
+                        distances.Remove(Pacman.Direction.down);
+                        continue;
+                    }
+
+                    PosY += delta_move;
+                    FacingDirection = Pacman.Direction.down;
+                    CurrentAction = Pacman.Button.move_down;
+                    break;
+                }
+
+                if (distances.Values.Min() == dist_left && distances.Keys.Contains(Pacman.Direction.left))
+                {
+                    if (PredictCollision(-delta_move, 0))
+                    {
+                        distances.Remove(Pacman.Direction.left);
+                        continue;
+                    }
+
+                    PosX -= delta_move;
+                    FacingDirection = Pacman.Direction.left;
+                    CurrentAction = Pacman.Button.move_left;
+                    break;
+                }
+
+                if (distances.Values.Min() == dist_right && distances.Keys.Contains(Pacman.Direction.right))
+                {
+                    if (PredictCollision(delta_move, 0))
+                    {
+                        distances.Remove(Pacman.Direction.right);
+                        continue;
+                    }
+
+                    PosX += delta_move;
+                    FacingDirection = Pacman.Direction.right;
+                    CurrentAction = Pacman.Button.move_right;
+                    break;
+                }
+            }
+
+            if (IsWithinWarp())
+            {
+                WarpEntity();
+            }
         }
+
+        public abstract Vector2 GetTarget();
     }
 
     public sealed class Blinky : Ghost
     {
-
+        public override Vector2 GetTarget()
+        {
+            return new Vector2(EntityManager.player.PosX, EntityManager.player.PosY);
+        }
     }
 
     public sealed class Pinky : Ghost
     {
-
+        public override Vector2 GetTarget()
+        {
+            return new Vector2(EntityManager.player.PosX, EntityManager.player.PosY);
+        }
     }
 
     public sealed class Inky : Ghost
     {
-
+        public override Vector2 GetTarget()
+        {
+            return new Vector2(EntityManager.player.PosX, EntityManager.player.PosY);
+        }
     }
 
     public sealed class Clyde : Ghost
     {
-
+        public override Vector2 GetTarget()
+        {
+            return new Vector2(EntityManager.player.PosX, EntityManager.player.PosY);
+        }
     }
 }
--- Game Files/Pacman.cs
+++ Game Files/Pacman.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -10,11 +11,11 @@ namespace Pacman
     {
         private readonly GraphicsDeviceManager graphics;
         private SpriteBatch spriteBatch;
-        public const int player_size = 12;
-        public const int tile_size = 16;
+        public const int entity_size = 23;
+        public const int tile_size = 24;
         public const int screen_width = 28;
         public const int screen_height = 36;
-        public const float scaling_factor = 1.7f;
+        public static float scaling_factor = 1.0f;
 
         // List of valid actions, these can have multiple keys assigned to them
         public enum Button
@@ -23,7 +24,7 @@ namespace Pacman
             move_down,
             move_left,
             move_right
-        } 
+        }
 
         // Dictionary that determines which keys correspond to which actions
         public static Dictionary<Button, List<Keys>> control_map = new Dictionary<Button, List<Keys>>()
@@ -32,6 +33,22 @@ namespace Pacman
             { Button.move_down, new List<Keys>() { Keys.S, Keys.Down } },
             { Button.move_left, new List<Keys>() { Keys.A, Keys.Left } },
             { Button.move_right, new List<Keys>() { Keys.D, Keys.Right } }
+        };
+
+        public enum Direction
+        {
+            up,
+            down,
+            left,
+            right
+        }
+
+        public static Dictionary<Direction, Direction> OppositeDir = new Dictionary<Direction, Direction>()
+        {
+            { Direction.up, Direction.down },
+            { Direction.down, Direction.up },
+            { Direction.left, Direction.right },
+            { Direction.right, Direction.left }
         };
 
         // Constructor
@@ -46,6 +63,7 @@ namespace Pacman
             base.Initialize();
             graphics.PreferredBackBufferHeight = (int)(tile_size * screen_height * scaling_factor);
             graphics.PreferredBackBufferWidth = (int)(tile_size * screen_width * scaling_factor);
+            Window.AllowUserResizing = true;
             graphics.ApplyChanges();
             TileManager.CreateGameMap(Content);
         }
@@ -64,6 +82,10 @@ namespace Pacman
 
         protected override void Update(GameTime gameTime)
         {
+            float sf1 = (float)(graphics.PreferredBackBufferHeight) / (tile_size * screen_height);
+            float sf2 = (float)(graphics.PreferredBackBufferWidth) / (tile_size * screen_width);
+            scaling_factor = Math.Min(sf1, sf2);
+
             foreach (Entity entity in EntityManager.GetEntityList())
             {
                 entity.Move();
@@ -74,7 +96,7 @@ namespace Pacman
         
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
             spriteBatch.Begin(SpriteSortMode.Immediate, null, SamplerState.PointClamp, null, null, null, Matrix.CreateScale(scaling_factor));
 
@@ -100,20 +122,6 @@ namespace Pacman
         public static bool IsButtonPressed(Pacman.Button button)
         {
             return Pacman.control_map[button].Any(x => Keyboard.GetState().IsKeyDown(x));
-        }
-
-        // Make sure only one direction is active at a time
-        public static bool IsOnlyOneDirectionPressed()
-        {
-            List<Pacman.Button> directions = new List<Pacman.Button>()
-            {
-                Pacman.Button.move_up,
-                Pacman.Button.move_down,
-                Pacman.Button.move_left,
-                Pacman.Button.move_right
-            };
-
-            return directions.Count(x => IsButtonPressed(x)) == 1;
         }
     }
 }
